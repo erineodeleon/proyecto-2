8cb9b410a703b5623002e3d323f1272a178072ee
erieneodeleon <erineodeleon5@gmail.com>
2020-04-22 21:31:39 -0600

    errores en los operadores, resta, multiplicacion, division

--- js/funciones.js
+++ js/funciones.js
@@ -1,3 +1,27 @@
 function limpiar() {
     document.getElementById("formulario").reset();
 }
+
+function sumar() {
+    var x = parseInt(document.getElementById("valor 1").value);
+    var y = parseInt(document.getElementById("valor 2").value);
+    document.getElementById("el-resultado").innerHTML = x + y;
+}
+
+function restar() {
+    var x = parseInt(document.getElementById("valor 1").Value);
+    var y = parseInt(document.getElementById("valor 2").Value);
+    document.getElementById("el-resultado").innerHTML = (x - y);
+}
+
+function multiplicar() {
+    var x = parseInt(document.getElementById("valor 1").Value);
+    var y = parseInt(document.getElementById("valor 2").Value);
+    document.getElementById("el-resultado").innerHTML = (x * y);
+}
+
+function dividir() {
+    var x = parseInt(document.getElementById("valor 1").Value);
+    var y = parseInt(document.getElementById("valor 2").Value);
+    document.getElementById("el-resultado").innerHTML = x / y;
+}
